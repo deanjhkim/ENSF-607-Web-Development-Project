ce81b493693babd5ea9499b76d5db0e388ca0621
Dean Kim <62639945+deanjhkim@users.noreply.github.com>
2020-12-10 18:43:50 -0700

    React preproject

--- README.DeanReact.md
+++ README.DeanReact.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
@@ -68,3 +69,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+=======
+# ENSF-607-Web-Development-Project
+Creating a Django/React project implementing specified functionality
+>>>>>>> 4e83d30c22ce9eb040c992735fc207a1dbd70b9c
--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import logo from './logo.svg';
 import './App.css';
-
+import 'bulma/css/bulma.css'
+import {useState} from "react";
 function App() {
+
+  
+
+  const [counter, setCounter ] = useState(0);
+
+  const [inputValue, setInputValue] = useState("");
+  
+  const decrement = () => {
+    setCounter(counter - 1);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.Key == 'Enter'){
+      setCounter(parseInt(inputValue));
+    }
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <div className = "container">
+        <div className = "columns is-multiline">
+          <div className = "columns is-full">
+            <div className = "notification">
+              <div className = "columns">
+                <div className = "column is-half">
+                  <div className="field has-addons">
+                    <div className="control">
+                      <input 
+                        className="input" 
+                        type="text"
+                        placeholder="Enter a number"
+                        value = {inputValue}
+                        onChange=  { (e)=> setInputValue(e.target.value)}
+                        onKeyDown = { (e)=> handleKeyDown(e.target.value)}
+                        
+                      />
+                    </div>
+                    <div className="control">
+                      <a className="button is-info"
+                        onClick = { () => setCounter(parseInt(inputValue))}
+                      >
+                        Assign
+                      </a>
+                    </div>
+                  </div>
+                  <div className="buttons has-addons">
+                    <button className="button is-primary"
+                    onClick = {() => setCounter (counter +1)}
+                    
+                    >
+                      Up
+                    </button>
+                    <button className="button is-warning" onClick = {decrement}>
+                      Down
+                    </button>
+                  </div>
+                </div>
+                <div className = "column is-half has-text-centered">
+                  <h1 className = "title">{counter}</h1>
+                </div>
+              
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
